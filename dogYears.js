64754010c61a57b42da2b45fad9d2e326da8329f
Sleepiii <95644163+Onlyizy@users.noreply.github.com>
2022-06-22 21:19:17 -0400

    Create dogYears.js
    Dogs mature at a faster rate than human beings. We often say a dog’s age can be calculated in “dog years” to account for their growth compared to a human of the same age. In some ways we could say, time moves quickly for dogs — 8 years in a human’s life equates to 45 years in a dog’s life. How old would you be if you were a dog?
    
    Here’s how you convert your age from “human years” to “dog years”:
    
    The first two years of a dog’s life count as 10.5 dog years each.
    Each year following equates to 4 dog years.
    Before you start doing the math in your head, let a computer take care of it! With your knowledge of math operators and variables, use JavaScript to convert your human age into dog years.

---
+++ dogYears.js
@@ -0,0 +1,28 @@
+//age var
+const myAge=29.5
+
+//early Years variable
+let earlyYears = 2
+
+//early years are multiplied by 10.5
+earlyYears *= 10.5
+
+//the rest of the years are stored in laterYears variable
+let laterYears= myAge-2
+
+//and count for 4 years each
+laterYears *= 4
+
+//For debugging/control purposes
+/*console.log(`The early years are: ${earlyYears} and the later years are: ${laterYears}`)*/
+ 
+// The sum of the early and later years gives us the total age in dog years
+myAgeInDogYears = laterYears + earlyYears
+
+//Storing my name in lower case letter
+let myName= "Isaac".toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+console.log()
+console.log("Fun fact: the longest a dog ever lived is 29.5 human years (131 dog years)")
